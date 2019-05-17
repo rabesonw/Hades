b653097959a1ae479d6513c854343dc2fe13d42a
weslie <weslie.rabeson@gmail.com>
2019-05-17 23:16:06 +0200

    Stream : attempt to implement streaming with aplayer (failed)

---
+++ public/js/aplayer.js
@@ -0,0 +1,44 @@
+// var colors = require("../../node_modules/color-thief/js");
+// // var playlist = require("./")
+
+var options = {
+    element: document.getElementById("aplayer"),
+
+    // listFolded: true,
+    // listMaxHeight: 100,
+
+    // autoplay: false,
+    // loop: "none",
+    // prelaod: "auto",
+    // mutex: true,
+
+    // fixed: true,
+    mini: true,
+
+    // order: "list",
+    
+    music: [
+        {
+            title: "just playing some pokemon blue",
+            author: "Seth Everman",
+            url: "/home/hoot/Music/just_playing_some_pokémon_blue.mp3",
+            pic: "/home/hoot/Pictures/blastoise.jpg",
+            theme: "#c4daff"
+        }
+    ]
+};
+
+const player = new APlayer(options);
+
+const setTheme = function(index) {
+    if(!player.list.audios[index].theme) {
+        colors.getColorAsync(player.list.audios[index].cover, function(color) {
+            player.theme(`rgb(${color[0]}, ${color[1]}, ${color[2]})`)
+        });
+    }
+};
+
+setTheme(player.list.index);
+player.on("listswitch", function(data) {
+    setTheme(data.index);
+});
